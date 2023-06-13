a0d91f202e4a4624cce1bfa65dde5a7f84e3fab7
IVAN <107381192+ivanchristiann@users.noreply.github.com>
2023-06-13 14:49:30 +0700

    Update create.blade.php

--- resources/views/tindakanPasien/create.blade.php
+++ resources/views/tindakanPasien/create.blade.php
@@ -1,9 +1,20 @@
 <style>
-    label{
+    label {
         margin-top: 15px;
         margin-bottom: 10px;
         color: black;
     }
+
+    #addTindakan{
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        align-content: center;
+        gap: 1em;
+        white-space: nowrap;
+    }
+
 </style>
 @extends('layout.sneat')
 
@@ -21,9 +32,6 @@
         <label for="exampleInputEmaill">Nama Pasien</label>
         <input type="text" name="namaDokter" class="form-control" id="namaDokter" aria-describedby="nameHelp">
 
-        <label for="exampleInputEmaill">Jenis Tindakan</label>
-        <input type="text" name="emailDokter" class="form-control" id="emailDokter" aria-describedby="nameHelp">
-
         <label for="exampleInputEmaill">Dokter</label>
         <div>
             <select class="form-select" aria-label="Default select example" name="categoryProduk" id="catProduk">
@@ -40,23 +48,34 @@
             <select class="form-select" aria-label="Default select example" name="categoryProduk" id="catProduk">
                 <option>-- Pilih Diagnosa --</option>
                 @foreach ($diagnosas as $diagnosa)
-                <option value="{{ $diagnosa->id }}">{{ $diagnosa->kode_diagnosa }} - {{ $diagnosa->nama_diagnosa }}</option>
+                <option value="{{ $diagnosa->id }}">{{ $diagnosa->kode_diagnosa }} - {{ $diagnosa->nama_diagnosa }}
+                </option>
                 @endforeach
             </select>
         </div>
-        
-        <label for="exampleInputEmaill">Tanggal Kunjungan</label>
-        <input type="datetime-local" name="singkatan" class="form-control" id="singkatan" aria-describedby="nameHelp">
 
-        <label for="exampleInputEmaill">Jumlah Tindakan</label>
-        <input type="number" name="singkatan" class="form-control" id="singkatan" aria-describedby="nameHelp">
+        <label for="exampleInputEmaill">Tanggal Kunjungan</label>
+        <input type="date" name="tanggalKunjungan" class="form-control" id="tanggalKunjungan"
+            aria-describedby="nameHelp">
+
+        <div id="addTindakan">
+            <label>Jenis Tindakan</label>
+            <div>
+                <select class="form-select" aria-label="Default select example" name="jenisTindakan" id="jenisTindakan">
+                    <option>-- Pilih Jenis Tindakan --</option>
+                    @foreach ($jenisTindakans as $jenisTindakan)
+                    <option value="{{ $jenisTindakan->id }}">{{ $jenisTindakan->nama_tindakan }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <label for="exampleInputEmaill">Jumlah Tindakan</label>
+            <input type="number" name="singkatan" class="form-control" id="singkatan" aria-describedby="nameHelp">
+        </div>
 
         <label for="exampleInputEmaill">Total Biaya</label>
         <input type="number" name="singkatan" class="form-control" id="singkatan" aria-describedby="nameHelp" step="1000">
     </div>
     <button type="submit" class="btn btn-primary" style="margin-top: 20px;">Submit</button>
-
-
 </form>
 
 @endsection
@@ -64,5 +83,5 @@
 @section('script')
 <script type="text/javascript">
 
- </script>
+</script>
 @endsection
