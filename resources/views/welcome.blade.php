83f8618598775fc096b091f775140b4e6877c2a2
ivanchristiann <ivan_christian_sby@yahoo.co.id>
2023-07-06 22:52:06 +0700

    Welcome Nama +Role

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,11 @@
 
 @section('menu')
 <div class="portlet-title">
-    <div style="display: inline-block; margin: 15px; font-size: 25px; font-weight: bold;">
-        Welcome
+    <div style="display: inline-block; margin-left: 15px;  margin-top: 15px; font-size: 25px; font-weight: bold;">
+        Welcome,  {{auth()->user()->name}}
+    </div>
+    <div style="margin-left: 15px; font-size: 18px; margin-bottom: 15px;">
+        {{ strtoupper(auth()->user()->role) }}
     </div>
 </div>
 @endsection
